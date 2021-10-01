1f708773554a4679acbbade8cbe639c0624d0b93
AnandaGouda <ananda_fw10_072@masai.school>
2021-10-01 16:33:01 +0530

    added all file

---
+++ ananda/models/enterprise.js
@@ -0,0 +1,20 @@
+const  mongoose  = require("mongoose")
+
+
+
+let enterpriseSchema = new mongoose.Schema({
+
+    name:{type:String},
+    phone:{type:String},
+    email:{type:String}
+
+})
+
+module.exports =  mongoose.model("enterprise", enterpriseSchema)
+
+
+
+
+
+
+
