d52bc1f39d14269ef5f1e0cd5a43d4f42123dc6a
Chandan Kumar Malik <83898344+stockchandu@users.noreply.github.com>
2021-10-05 21:30:25 +0530

    Delete kapil directory

--- kapil/Merge_File/configs/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const connect=()=>{
-    // return mongoose.connect("mongodb://127.0.0.1:27017/porterdatabase")
-    return mongoose.connect("mongodb+srv://chandan:c123456789@cluster0.sl7vx.mongodb.net/porterdatabase?retryWrites=true&w=majority")
-}
-
-module.exports=connect
