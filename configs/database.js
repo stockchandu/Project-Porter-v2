1550217c5cc43a5bb1a526568f4d93788ca60304
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2023-06-10 17:34:20 +0530

    Update database.js

--- configs/database.js
+++ configs/database.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose")
 
-const connect=()=>{
-    return mongoose.connect("mongodb+srv://chandan:c123456789@cluster0.sl7vx.mongodb.net/porterdatabase?retryWrites=true&w=majority")
-}
+
 
 module.exports=connect
