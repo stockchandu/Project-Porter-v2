b197b3da371479bac40bab2743ae2976ac5287ea
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2023-07-14 18:57:22 +0530

    Update enterprise.model.js

--- model/enterprise.model.js
+++ model/enterprise.model.js
@@ -1,4 +1,14 @@
+const  mongoose  = require("mongoose");
+let enterpriseSchema = new mongoose.Schema({
+    name:{type:String},
+    phone:{type:String},
+    email:{type:String}
 
+},{
+    versionKey:false ,
+    timestamps:true, 
+})
+module.exports =  mongoose.model("enterprise", enterpriseSchema)
 
 
 
