22db84a89fc4846718c7071b141aaa55a5a8be53
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2023-07-19 21:07:43 +0530

    Update enterprise.controller.js

--- controller/enterprise.controller.js
+++ controller/enterprise.controller.js
@@ -1,1 +1,25 @@
 
+const express= require("express");
+const router= express.Router();
+const Enterprise = require("../model/enterprise.model");
+router.post("/", async (req, res)=>{
+    try{
+
+        const registerEnterprise = new Enterprise({
+            name:req.body.name_person,
+            phone:req.body.phone_num,
+            email:req.body.email_id
+        })
+
+       await registerEnterprise.save()
+       setTimeout(()=>{
+        res.render("for_enterprise")
+       },10000) 
+       
+    } catch(error){
+        res.status(400).send(error);
+    }
+    
+})
+
+module.exports=router;
